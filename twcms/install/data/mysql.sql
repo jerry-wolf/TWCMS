b2f212ef2576d575211803ef2469152d4d4ffed6
lmath <l5h5t7@gmail.com>
2023-12-15 14:38:15 +0000

    kv 表的 v 使用 json 类型

--- twcms/install/data/mysql.sql
+++ twcms/install/data/mysql.sql
@@ -477,7 +477,7 @@ CREATE TABLE pre_cms_photo_tag_data (
 DROP TABLE IF EXISTS pre_kv;
 CREATE TABLE pre_kv (
   `k` char(32) NOT NULL DEFAULT '',			# 键名
-  `v` text NOT NULL DEFAULT '',				# 数据
+  `v` json NOT NULL DEFAULT '',				# 数据
   `expiry` int unsigned NOT NULL DEFAULT '0',		# 过期时间
   PRIMARY KEY(k)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
